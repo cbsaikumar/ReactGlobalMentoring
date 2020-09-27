44519cac8d8b385a6ca45df904578fab003065c6
Bhargava Chittapudi <Bhargava_Chittapudi@epam.com>
2020-09-27 17:32:24 +0530

    hometask 3 completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,65 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
-const mode = process.env.NODE_ENV;
+const mode = process.env.NODE_ENV
 
-const isProd = mode === "production";
+const isProd = mode === 'production'
 
 module.exports = {
-  mode,
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
+    mode,
+    entry: './src/index.jsx',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                },
+            },
         ],
-      },
+    },
+    devtool: isProd ? false : 'inline-source-map',
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
     ],
-  },
-  devtool: isProd ? false : 'inline-source-map',
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html",
-    }),
-  ],
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx',
-    ],
-  },
-  optimization: {
-    removeEmptyChunks: isProd,
-    mergeDuplicateChunks : isProd
-  }
-};
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    optimization: {
+        removeEmptyChunks: isProd,
+        mergeDuplicateChunks: isProd,
+    },
+}
