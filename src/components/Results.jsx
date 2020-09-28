7ef7b10f7b1872acd6caf7d392449c8b427019f1
Bhargava Chittapudi <Bhargava_Chittapudi@epam.com>
2020-09-28 08:48:26 +0530

    hometask-4 completed

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,20 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import CONSTANTS from '../constants/constants';
+import React from 'react'
+import PropTypes from 'prop-types'
+import CONSTANTS from '../constants/constants'
 
 const Results = ({ items }) => {
-  return (
-    <div className="bar grey lighten-1">
-      <h6 className="white-text text-darken-2 grey lighten-1 filter-bar left">{items + " " + CONSTANTS.MOVIES_FOUND}</h6>
-      <h6 className="white-text text-darken-2 grey lighten-1 filter-bar right">{CONSTANTS.RATING}</h6>
-      <h6 className="white-text text-darken-2 grey lighten-1 filter-bar right">{CONSTANTS.RELEASE}</h6>
-      <h6 className="white-text text-darken-2 grey lighten-1 filter-bar right">{CONSTANTS.SORT}</h6>
-    </div>
-  )
+    return (
+        <>
+            <div className="bar grey lighten-1">
+                <span className="left">
+                    <h6 className="white-text text-darken-2 grey lighten-1 filter-bar left">
+                        ALL
+                    </h6>
+                    <h6 className="white-text text-darken-2 grey lighten-1 filter-bar left">
+                        DOCUMENTARY
+                    </h6>
+                    <h6 className="white-text text-darken-2 grey lighten-1 filter-bar left">
+                        COMEDY
+                    </h6>
+                    <h6 className="white-text text-darken-2 grey lighten-1 filter-bar left">
+                        HORROR
+                    </h6>
+                    <h6 className="white-text text-darken-2 grey lighten-1 filter-bar left">
+                        CRIME
+                    </h6>
+                </span>
+                <span className="rigt">
+                    <h6 className="white-text text-darken-2 grey lighten-1 filter-bar right">
+                        {CONSTANTS.RATING}
+                    </h6>
+                    <h6 className="white-text text-darken-2 grey lighten-1 filter-bar right">
+                        {CONSTANTS.RELEASE}
+                    </h6>
+                    <h6 className="white-text text-darken-2 grey lighten-1 filter-bar right">
+                        {CONSTANTS.SORT}
+                    </h6>
+                </span>
+            </div>
+            <hr />
+            <div className="bar grey lighten-1">
+                <h6 className="white-text text-darken-2 grey lighten-1 filter-bar left">
+                    {items + ' ' + CONSTANTS.MOVIES_FOUND}
+                </h6>
+            </div>
+        </>
+    )
 }
 
 Results.propTypes = {
-  items: PropTypes.number
-};
+    items: PropTypes.number,
+}
 
-export default Results;
+export default Results
