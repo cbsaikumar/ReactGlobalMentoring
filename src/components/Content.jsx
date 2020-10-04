a2a2d631931738a1c9df61afab92319b1c5bb123
Bhargava Chittapudi <Bhargava_Chittapudi@epam.com>
2020-10-04 17:48:43 +0530

    hometask-6 completed

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,27 +3,20 @@ import PropTypes from 'prop-types'
 import MoviesList from './MoviesList'
 import ErrorMessage from './ErrorMessage'
 import CONSTANTS from '../constants/constants'
+import { useSelector } from 'react-redux'
 
-const Content = ({ movies, setSelectedMovie }) => {
+const Content = () => {
+    const movies = useSelector((state) => state.movies.data)
+
     const moviesFound = movies.length > 0
     return (
         <React.Fragment>
-            {moviesFound && (
-                <MoviesList
-                    movies={movies}
-                    setSelectedMovie={setSelectedMovie}
-                />
-            )}
+            {moviesFound && <MoviesList movies={movies} />}
             {!moviesFound && (
                 <ErrorMessage message={CONSTANTS.NO_FILMS_FOUND} />
             )}
         </React.Fragment>
     )
-}
-
-Content.propTypes = {
-    movies: PropTypes.array,
-    setSelectedMovie: PropTypes.func,
 }
 
 export default Content
